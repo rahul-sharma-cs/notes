1bdae82b4ae1f9a46379aa4a906354c83602c878
Rahul Sharma <rahul2005great@gmail.com>
2025-06-21 21:04:06 -0400

    creating pages (auth, home)

---
+++ website/auth.py
@@ -0,0 +1,17 @@
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return '<h1>Login Page</h1>'
+
+
+@auth.route('/logout')
+def logout():
+    return '<h1>Logout Page</h1>'
+
+
+@auth.route('/signup')
+def signup():
+    return '<h1>Signup Page</h1>'
