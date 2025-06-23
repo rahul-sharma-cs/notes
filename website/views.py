b565f1855ca6c65209ccdd711f37e957d42249e8
Rahul Sharma <rahul2005great@gmail.com>
2025-06-23 15:45:17 -0400

    Final Push

--- website/views.py
+++ website/views.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
 from .models import Note
 from . import db
+import json
+import jsonify
 
 views = Blueprint('views', __name__)
 
@@ -17,6 +19,16 @@ def home():
             db.session.add(new_note)
             db.session.commit()
             flash('Note added!', category='success')
-    notes = Note.query.filter_by(user_id=current_user.id).all()
-    return render_template('home.html', user=current_user, notes=notes)
+
     return render_template('home.html')
+
+@views.route('/delete-note', methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteId = note['noteId']
+    note = Note.query.get(noteId)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
